ff5e805fd7fdf41b82ecd56e3fdb129779b031fd
iyeonggyu0 <iyeonggyu0@gmail.com>
2025-01-21 02:04:36 +0900

    commit

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,18 @@
+// babel.config.js 예시
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: [
+    // 다른 프리셋
+  ],
   plugins: [
     [
       "module-resolver",
       {
         root: ["./src"],
         alias: {
-          "@": "./src",
+          // 필요한 경우 별칭 설정
         },
       },
     ],
+    // 다른 플러그인
   ],
 };
