e124b7d46552605164ac903a80ba1a3c84f54ad8
iyeonggyu0 <iyeonggyu0@gmail.com>
2025-01-21 01:43:57 +0900

    commit

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
-// jest.config.js
 module.exports = {
-  moduleDirectories: ["node_modules", "src"],
+  // 기존 설정들...
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1", // 예시: 'src' 디렉터리를 별칭으로 설정
+  },
 };
